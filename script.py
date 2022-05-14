1a930a4e7b60a18402c4d35cc0be57eaf5641378
Sagar <sgrydv0@gmail.com>
2022-05-14 13:31:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import platform
- 
+import os
+from datetime import date, timedelta
 my_system = platform.uname()
  
 print(f"System: {my_system.system}")
@@ -8,3 +9,8 @@ print(f"Release: {my_system.release}")
 print(f"Version: {my_system.version}")
 print(f"Machine: {my_system.machine}")
 print(f"Processor: {my_system.processor}")
+
+
+
+Actualdate = date.today().strftime("%m-%d-%y")
+print(Actualdate)
