e9eab4d292efb9517ed4f0d6c074cb96545f736d
Sagar <sgrydv0@gmail.com>
2022-05-15 20:02:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,40 +12,10 @@ print(f"Version: {my_system.version}")
 print(f"Machine: {my_system.machine}")
 print(f"Processor: {my_system.processor}")
 
-
-
 Actualdate = date.today().strftime("%m-%d-%y")
 print(Actualdate)
 
-def getListOfProcessSortedByMemory():
-    '''
-    Get list of running process sorted by Memory Usage
-    '''
-    listOfProcObjects = []
-    # Iterate over the list
-    for proc in psutil.process_iter():
-       try:
-           # Fetch process details as dict
-           pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-           pinfo['vms'] = proc.memory_info().vms / (1024 * 1024)
-           # Append dict to list
-           listOfProcObjects.append(pinfo);
-       except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-           pass
-    # Sort list of dict by key vms i.e. memory usage
-    listOfProcObjects = sorted(listOfProcObjects, key=lambda procObj: procObj['vms'], reverse=True)
-    return listOfProcObjects
 def main():
-    print("*** Iterate over all running process and print process ID & Name ***")
-    # Iterate over all running process
-    for proc in psutil.process_iter():
-        try:
-            # Get process name & pid from process object.
-            processName = proc.name()
-            processID = proc.pid
-            print(processName , ' ::: ', processID)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
     print('*** Create a list of all running processes ***')
     listOfProcessNames = list()
     # Iterate over all running processes
@@ -57,12 +27,8 @@ def main():
     # Iterate over the list of dictionary and print each elem
     for elem in listOfProcessNames:
         print(elem)
-    print('*** Top 5 process with highest memory usage ***')
-    listOfRunningProcess = getListOfProcessSortedByMemory()
-    for elem in listOfRunningProcess[:5] :
-        print(elem)
-        
-        # Writng logs into a file----------------------     
+
+        # Writng logs into a file----------------------
         # get current date and time
         current_datetime = datetime.now()
         # convert datetime obj to string
@@ -71,7 +37,7 @@ def main():
         # create a file object along with extension
         file_name = "logs-"+str_current_datetime+".txt"
         file = open(file_name, 'w')
-        file.writelines(elem)  
+        file.writelines(elem)
         file.close()
 
 if __name__ == '__main__':
